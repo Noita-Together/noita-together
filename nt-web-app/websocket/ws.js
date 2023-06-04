94a7a26d20db74daaf00e39cd06ebfed97d8ffaf
Skye Telman <b.telman01@gmail.com>
2023-06-03 23:48:42 -0400

    Validate that the token is valid on the backend

--- nt-web-app/websocket/ws.js
+++ nt-web-app/websocket/ws.js
@@ -1,8 +1,11 @@
 const URL = require("url")
 const path = require("path")
 const jwt = require("jsonwebtoken")
+const jwkToPem = require('jwk-to-pem');
 
 const Lobby = require("./lobby")
+const axios = require("axios");
+let cachedJWKS = undefined
 
 module.exports = (server) => {
     server.on('upgrade', async (req, socket, head) => {
@@ -25,12 +28,67 @@ module.exports = (server) => {
     })
 }
 
-const getUser = async (token)=>{
-    if(!token) return null
-    const jwtObject = jwt.decode(token)
-    //TODO https://id.twitch.tv/oauth2/.well-known/openid-configuration to validate the JWT is valid
-    return {
-        id: jwtObject.sub,
-        display_name: jwtObject['preferred_username']
-    }
+const fetchJWKS = async () => {
+    console.log('Fetch JWKS...')
+    const response = await axios.get('https://id.twitch.tv/oauth2/keys')
+    console.log('Fetched!')
+    return response.data
+}
+
+const verifyJwt = (jwtToken, jwks) => {
+    console.log('Verify JWT...')
+    return new Promise((resolve, reject) => {
+        const decodedToken = jwt.decode(jwtToken, {complete: true});
+
+        const {header} = decodedToken;
+        const {kid} = header;
+
+        const jwk = jwks.keys.find(key => key.kid === kid);
+
+        if (!jwk) {
+            reject(new Error(`JWK with kid ${kid} not found`));
+        }
+
+        const publicKey = {
+            alg: jwk.alg,
+            e: jwk.e,
+            kid: jwk.kid,
+            kty: jwk.kty,
+            n: jwk.n,
+            use: jwk.use,
+        };
+
+        const options = {
+            algorithms: [publicKey.alg],
+        };
+
+        // Convert JWK to PEM format
+        const pemPublicKey = jwkToPem(publicKey);
+        jwt.verify(jwtToken, pemPublicKey, options, (err, decoded) => {
+            if (err) {
+                console.log('Error!')
+                reject(new Error(`JWT verification failed: ${err.message}`));
+            } else {
+                resolve(decoded);
+            }
+        });
+    });
+}
+
+const getUser = async (token) => {
+    if (!token) return null
+    if (!cachedJWKS)
+        cachedJWKS = await fetchJWKS()
+    return verifyJwt(token, cachedJWKS)
+        .then((jwtObject) => {
+            return {
+                id: jwtObject.sub,
+                display_name: jwtObject['preferred_username']
+            }
+        })
+        .catch((e) => {
+            console.error('We failed to validate JWT :(. No user!')
+            console.error(e)
+            return null
+        })
 }
