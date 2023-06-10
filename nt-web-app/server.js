1769ccf9eb4defd4d9ebef7a398f11c4a609a18d
Skye Telman <b.telman01@gmail.com>
2023-06-09 20:37:15 -0400

    Remove extra handlers

--- nt-web-app/server.js
+++ nt-web-app/server.js
@@ -18,37 +18,7 @@ app.prepare().then(() => {
             const parsedUrl = parse(req.url, true);
             const { pathname, query } = parsedUrl;
 
-            if (pathname === '/a') {
-                res.end('<!DOCTYPE html>\n' +
-                    '<html>\n' +
-                    '<head>\n' +
-                    '  <style>\n' +
-                    '    body {\n' +
-                    '      background-color: black;\n' +
-                    '      display: flex;\n' +
-                    '      justify-content: center;\n' +
-                    '      align-items: center;\n' +
-                    '      height: 100vh;\n' +
-                    '      margin: 0;\n' +
-                    '    }\n' +
-                    '\n' +
-                    '    h1 {\n' +
-                    '      color: white;\n' +
-                    '      text-align: center;\n' +
-                    '      font-family: Arial, sans-serif;\n' +
-                    '      font-size: 24px;\n' +
-                    '    }\n' +
-                    '  </style>\n' +
-                    '</head>\n' +
-                    '<body>\n' +
-                    '  <h1>you can close this</h1>\n' +
-                    '</body>\n' +
-                    '</html>\n')
-            } else if (pathname === '/b') {
-                await app.render(req, res, '/b', query);
-            } else {
-                await handle(req, res, parsedUrl);
-            }
+            await handle(req, res, parsedUrl);
         } catch (err) {
             console.error('Error occurred handling', req.url, err);
             res.statusCode = 500;
