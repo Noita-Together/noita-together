9aad0df043e66c088af1265ab1f1c6b75673cc2a
Kabby <1925023+tehkab@users.noreply.github.com>
2023-08-05 21:55:26 -0400

    add code for dynamically generating hourglass triggers

--- noita_mod/core/files/append/snowcastle_cavern.lua
+++ noita_mod/core/files/append/snowcastle_cavern.lua
@@ -1,12 +1,19 @@
+dofile("mods/noita-together/files/scripts/hourglass_events.lua")
+
 local _spawn_hourglass_blood = spawn_hourglass_blood
 
 function spawn_hourglass_blood(x, y)
     if (GameHasFlagRun("NT_GAMEMODE_CO_OP")) then
-        EntityLoad( "mods/noita-together/files/entities/hourglass_ambrosia.xml", x, y+2 )
+        --GamePrint("" .. type(hourglassEventHandlers["charm"]) .. " " .. type(hourglassEventHandlers["berserk"]))
+        for e,v in pairs(hourglassEventHandlers) do
+            --GamePrint("hourglass " .. e .. " " .. v["material"])
+            CreateHourglassEntity(x, y+2, e, v["material"])
+        end
+        --[[EntityLoad( "mods/noita-together/files/entities/hourglass_ambrosia.xml", x, y+2 )
         EntityLoad( "mods/noita-together/files/entities/hourglass_berserk.xml", x, y )
         EntityLoad( "mods/noita-together/files/entities/hourglass_charm.xml", x, y )
         EntityLoad( "mods/noita-together/files/entities/hourglass_confusion.xml", x, y )
-        EntityLoad( "mods/noita-together/files/entities/hourglass_speed.xml", x, y )
+        EntityLoad( "mods/noita-together/files/entities/hourglass_speed.xml", x, y )]]--
     end
     _spawn_hourglass_blood(x, y)
 end
--- noita_mod/core/files/scripts/hourglass_events.lua
+++ noita_mod/core/files/scripts/hourglass_events.lua
@@ -1,8 +1,36 @@
+dofile_once("mods/noita-together/files/store.lua")
+dofile_once("mods/noita-together/files/scripts/json.lua")
+
+--material area checker callback from material_area_checker_component
+function material_area_checker_success( pos_x, pos_y )
+	local entity_id = GetUpdatedEntityID()
+	local x,y = EntityGetTransform(entity_id)
+	EntitySetComponentsWithTagEnabled(entity_id, "enabled_by_liquid", true)
+	EntitySetComponentsWithTagEnabled(entity_id, "disabled_by_liquid", false)
+	
+	-- kill others
+	for _,v in pairs(EntityGetInRadiusWithTag(x, y, 150, "hourglass_entity")) do
+		if v ~= entity_id then EntityKill(v) end
+    end
+
+    --get my effect name
+    local variable_storage_component = EntityGetFirstComponentIncludingDisabled(entity_id, "VariableStorageComponent")
+    local effect = ComponentGetValue2(variable_storage_component, "value_string")
+
+    local queue = json.decode(NT.wsQueue)
+    table.insert(queue, {event="CustomModEvent", payload={name="SecretHourglass", effect=effect}})
+    NT.wsQueue = json.encode(queue)
+
+    --TODO customizable message here
+    GamePrintImportant("$noitatogether_hourglass_gave_boon", "")
+end
+
 --register hourglass events
-function RegisterHourglassEvent(effect, handler, force)
+function RegisterHourglassEvent(effect, material, handler, force)
     --refuse to overwrite existing handler unless force true
     if hourglassEventHandlers[effect] ~= nil and not force then
-        --todo emit a warning
+        --todo emit a warning?
+        --print_error("(NT) Attempted to overwrite hourglass handler \"" .. effect .. "\"!") --???
         return false
     end
 
@@ -14,7 +42,7 @@ function RegisterHourglassEvent(effect, handler, force)
     end
 
     --assign it
-    hourglassEventHandlers[effect] = handler
+    hourglassEventHandlers[effect] = { material = material, handler = handler }
 
     --return true, success
     return true
@@ -22,15 +50,32 @@ end
 
 --handle an hourglass event
 function HandleHourglassEvent(data)
-    local handler = hourglassEventHandlers[data.effect];
+    local entry = hourglassEventHandlers[data.effect];
     --ignore if there is not a function for data.effect
-    if ( handler == nil or type(handler) ~= "function" ) then
+    if entry == nil or type(entry) ~= "table" or type(entry["handler"]) ~= "function" then
         --Emit a warning and do nothing???
         --todo something more graceful
         GamePrint("(NT) Warning: Unrecognized hourglass event \"" .. data.effect .. "\" !")
     else
         --run the registered handler for this effect class
-        handler(GetPlayer(), PlayerList[data.userId].name, data)
+        GamePrint("(NT) Exec hourglass event \"" .. data.effect .. "\"")
+        entry["handler"](GetPlayer(), PlayerList[data.userId].name, data)        
+    end
+end
+
+--create an area checker entity for an hourglass effect
+function CreateHourglassEntity(x, y, effect, material)
+    GamePrint("creating hourglass entity \"" .. effect .. "\":\"" .. material .. "\"")
+    if effect ~= nil and material ~= nil then
+        local effect_entity = EntityLoad("mods/noita-together/files/entities/hourglass_effect_trigger.xml", x, y)
+        local material_area_checker_component = EntityGetFirstComponentIncludingDisabled(effect_entity, "MaterialAreaCheckerComponent")
+        mat_type = CellFactory_GetType(material)
+        ComponentSetValue2(material_area_checker_component, "material", mat_type)
+        ComponentSetValue2(material_area_checker_component, "material2", mat_type)
+        local variable_storage_component = EntityGetFirstComponentIncludingDisabled(effect_entity, "VariableStorageComponent")
+        ComponentSetValue2(variable_storage_component, "value_string", effect)
+    else
+        --ignore, TODO throw warning
     end
 end
 
@@ -39,7 +84,7 @@ if not hourglassEventHandlers then
     hourglassEventHandlers = {}
 
     --DEFAULT NT HOURGLASS EVENTS
-    assert(RegisterHourglassEvent("ambrosia", function(player, triggered_by_name, data)
+    assert(RegisterHourglassEvent("ambrosia", "magic_liquid_protection_all", function(player, triggered_by_name, data)
         local x, y = GetPlayerOrCameraPos()
         local effect_entity = EntityLoad("mods/noita-together/files/effects/weak_ambrosia.xml", x, y)
         EntityAddChild(player, effect_entity)
@@ -54,7 +99,7 @@ if not hourglassEventHandlers then
         GamePrintImportant(GameTextGet("$noitatogether_hourglass_boost_title", triggered_by_name), "$noitatogether_hourglass_ambrosia_subtitle")
     end))
 
-    assert(RegisterHourglassEvent("berserk", function(player, triggered_by_name, data)
+    assert(RegisterHourglassEvent("berserk", "magic_liquid_berserk", function(player, triggered_by_name, data)
         local effect_entity = LoadGameEffectEntityTo(player, "data/entities/misc/effect_damage_multiplier.xml")
         local effect_comp = EntityGetFirstComponent(effect_entity, "GameEffectComponent")
         ComponentSetValue2(effect_comp, "frames", 60*120)
@@ -69,13 +114,13 @@ if not hourglassEventHandlers then
         GamePrintImportant(GameTextGet("$noitatogether_hourglass_boost_title", triggered_by_name), "$noitatogether_hourglass_berserk_subtitle")
     end))
 
-    assert(RegisterHourglassEvent("charm", function(player, triggered_by_name, data)
+    assert(RegisterHourglassEvent("charm", "magic_liquid_charm", function(player, triggered_by_name, data)
         local x, y = GetPlayerOrCameraPos()
         EntityLoad("data/entities/projectiles/deck/regeneration_field_long.xml", x, y)
         GamePrintImportant(GameTextGet("$noitatogether_hourglass_boon_title", triggered_by_name), "")
     end))
 
-    assert(RegisterHourglassEvent("confusion", function(player, triggered_by_name, data)
+    assert(RegisterHourglassEvent("confusion", "material_confusion", function(player, triggered_by_name, data)
         local fungi = CellFactory_GetType("fungi")
         if (player ~= nil) then
             EntityIngestMaterial( player, fungi, 300 )
@@ -90,7 +135,7 @@ if not hourglassEventHandlers then
         end
     end))
 
-    assert(RegisterHourglassEvent("speed", function(player, triggered_by_name, data)
+    assert(RegisterHourglassEvent("speed", "magic_liquid_movement_faster", function(player, triggered_by_name, data)
         local effect_entity = LoadGameEffectEntityTo(player, "data/entities/misc/effect_movement_faster.xml")
         local effect_comp = EntityGetFirstComponent(effect_entity, "GameEffectComponent")
         ComponentSetValue2(effect_comp, "frames", 60*45)
@@ -104,22 +149,4 @@ if not hourglassEventHandlers then
         })
         GamePrintImportant(GameTextGet("$noitatogether_hourglass_boost_title", triggered_by_name), "$noitatogether_hourglass_speed_subtitle")
     end))
-
-    --NEW NT HOURGLASS EFFECTS AUGUST 2023
-    --add an extra max hp perk from healthium
-    --[[assert(RegisterHourglassEvent("healthium"), function(player, triggered_by_name, data)
-        --TODO
-    end)]]--
-
-    --Some sort of debuff from diminution
-    --[[assert(RegisterHourglassEvent("diminution"), function(player, triggered_by_name, data)
-        --Debuff of some sort?
-        --TODO
-    end)]]--
-
 end
-
---ideas:
---something to give people free max hp buff (should be rare; healthium? probably not LC tho)
---something for diminution - even if it's strictly negative?
---something for instant deathium for the memez - but probably not anything immediately dangerous?
