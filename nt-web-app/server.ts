2f5a99d55d0dfc6bbc6e51fe9e2962346923f3a7
Skye Telman <b.telman01@gmail.com>
2023-08-05 20:20:45 -0400

    WIP load saved stats files

--- nt-web-app/server.ts
+++ nt-web-app/server.ts
@@ -3,13 +3,16 @@ dotenv.config();
 
 import * as http from 'http';
 import next from "next";
-import {NoitaTogetherWebsocket} from './websocket';
-import {parse} from "url";
-import {getServerAccessToken} from "./utils/TwitchUtils";
+import { NoitaTogetherWebsocket } from './websocket';
+import { parse } from "url";
+import { getServerAccessToken } from "./utils/TwitchUtils";
+import fs from 'fs';
+import path from 'path';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
 const port = 3000;
+
 // when using middleware `hostname` and `port` must be provided below
 let tokensPromise = getServerAccessToken()
 
@@ -22,13 +25,34 @@ app.prepare().then(async () => {
     websocket.startServer()
     http.createServer(async (req, res) => {
         try {
-            if(!req.url) return
-            // Be sure to pass `true` as the second argument to `url.parse`.
-            // This tells it to parse the query portion of the URL.
-            const parsedUrl = parse(req.url, true);
-            // const { pathname, query } = parsedUrl;
+            if (!req.url) return
 
-            await handle(req, res, parsedUrl);
+            const parsedUrl = parse(req.url, true);
+            const { pathname, query } = parsedUrl;
+
+            // Check if the URL matches the desired pattern
+            if (pathname === '/[room_id]/[session_id]/stats') {
+                const room_id = query.room_id as string;
+                const session_id = query.session_id as string;
+
+                // Read the HTML file and serve it
+                const filePath = path.join(__dirname, `.storage/stats/${room_id}/${session_id}/stats-final.html`);
+                fs.readFile(filePath, 'utf-8', (err, data) => {
+                    if (err) {
+                        console.error('Error reading file:', err);
+                        res.statusCode = 404;
+                        res.end('404 stats not found');
+                    } else {
+                        // Replace any dynamic values in the HTML file with room_id and session_id
+                        // const modifiedData = data.replace('[room_id]', room_id).replace('[session_id]', session_id);
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(data);
+                    }
+                });
+            } else {
+                // For other URLs, let Next.js handle them
+                await handle(req, res, parsedUrl);
+            }
         } catch (err) {
             console.error('Error occurred handling', req.url, err);
             res.statusCode = 500;
