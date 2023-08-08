5c9b42b52f4ff517464f2eb7e47671860d64e3d1
Kabby <1925023+tehkab@users.noreply.github.com>
2023-08-08 19:24:12 -0400

    remove more commented out hourglass code

--- noita_mod/core/files/append/snowcastle_cavern.lua
+++ noita_mod/core/files/append/snowcastle_cavern.lua
@@ -9,11 +9,6 @@ function spawn_hourglass_blood(x, y)
             --GamePrint("hourglass " .. e .. " " .. v["material"])
             CreateHourglassEntity(x, y+2, e, v["material"])
         end
-        --[[EntityLoad( "mods/noita-together/files/entities/hourglass_ambrosia.xml", x, y+2 )
-        EntityLoad( "mods/noita-together/files/entities/hourglass_berserk.xml", x, y )
-        EntityLoad( "mods/noita-together/files/entities/hourglass_charm.xml", x, y )
-        EntityLoad( "mods/noita-together/files/entities/hourglass_confusion.xml", x, y )
-        EntityLoad( "mods/noita-together/files/entities/hourglass_speed.xml", x, y )]]--
     end
     _spawn_hourglass_blood(x, y)
 end
