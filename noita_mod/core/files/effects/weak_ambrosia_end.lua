6823048be75250ea4243e5a7106804eec117d0c5
Kabby <1925023+tehkab@users.noreply.github.com>
2023-11-06 18:39:47 -0500

    fix bad ComponentObjectSetValue2, remove old commented code

--- noita_mod/core/files/effects/weak_ambrosia_end.lua
+++ noita_mod/core/files/effects/weak_ambrosia_end.lua
@@ -6,41 +6,17 @@ local x, y = EntityGetTransform( entity_id )
 
 if ( player_id ~= NULL_ENTITY ) and ( entity_id ~= player_id ) then
 	local comp = EntityGetFirstComponent( player_id, "DamageModelComponent" )
-	--local comps = EntityGetComponent( entity_id, "VariableStorageComponent" )
 	
+	--add physics_hit and curse resists; cannot add holy yet (doesnt exist in DamageModelComponent???)
+	--some weird esoteric damage types cant be handled this way https://noita.wiki.gg/wiki/Damage_Types#Esoteric_Damage_Types
+	--order them in the same way the game does
 	local resist_names = { "melee", "projectile", "explosion", "electricity", "fire", "drill", "slice", "ice", "physics_hit", "radioactive", "poison", "overeating", "curse"}
-	--local result = ""
 	
 	for _,res in ipairs(resist_names) do
 		local value = ComponentObjectGetValue2(comp, "damage_multipliers", res)
 		if value then
 			value = value * 2.0
-			ComponentObjectSetValue2(comp, "damage_multipliers", value)
+			ComponentObjectSetValue2(comp, "damage_multipliers", res, value)
 		end
 	end
-	
-	--[[if ( comp ~= nil ) and ( comps ~= nil ) then
-		for i,v in ipairs( comps ) do
-			local n = ComponentGetValue2( v, "name" )
-			
-			if ( n == "resis_data" ) then
-				result = ComponentGetValue2( v, "value_string" )
-				break
-			end
-		end
-		
-		if ( #result > 0 ) then
-			local data = {}
-			
-			for r in string.gmatch( result, "%S+" ) do
-				table.insert( data, r )
-			end
-			
-			for a,b in ipairs( resists ) do
-				local r = tostring( data[a] ) or "1.0"
-				
-				ComponentObjectSetValue( comp, "damage_multipliers", b, r )
-			end
-		end
-	end]]--
 end
--- noita_mod/core/files/effects/weak_ambrosia_start.lua
+++ noita_mod/core/files/effects/weak_ambrosia_start.lua
@@ -6,44 +6,17 @@ local x, y = EntityGetTransform( entity_id )
 
 if ( player_id ~= NULL_ENTITY ) and ( entity_id ~= player_id ) then
 	local comp = EntityGetFirstComponent( player_id, "DamageModelComponent" )
-	--local comps = EntityGetComponent( entity_id, "VariableStorageComponent" )
 	
 	--add physics_hit and curse resists; cannot add holy yet (doesnt exist in DamageModelComponent???)
 	--some weird esoteric damage types cant be handled this way https://noita.wiki.gg/wiki/Damage_Types#Esoteric_Damage_Types
 	--order them in the same way the game does
 	local resist_names = { "melee", "projectile", "explosion", "electricity", "fire", "drill", "slice", "ice", "physics_hit", "radioactive", "poison", "overeating", "curse"}
-	--local result = ""
 	
 	for _,res in ipairs(resist_names) do
 		local value = ComponentObjectGetValue2(comp, "damage_multipliers", res)
 		if value then
 			value = value * 0.5
-			ComponentObjectSetValue2(comp, "damage_multipliers", value)
+			ComponentObjectSetValue2(comp, "damage_multipliers", res, value)
 		end
 	end
-
-	--[[if ( comp ~= nil ) and ( comps ~= nil ) then
-		for a,b in ipairs( resists ) do
-			local r = tostring(ComponentObjectGetValue( comp, "damage_multipliers", b ))
-			
-			result = result .. r
-			
-			if ( a < #resists ) then
-				result = result .. " "
-			end
-			
-			ComponentObjectSetValue( comp, "damage_multipliers", b, "0.5" )
-		end
-	
-		if ( #result > 0 ) then
-			for i,v in ipairs( comps ) do
-				local n = ComponentGetValue2( v, "name" )
-				
-				if ( n == "resis_data" ) then
-					ComponentSetValue2( v, "value_string", result )
-					break
-				end
-			end
-		end
-	end]]--
 end
