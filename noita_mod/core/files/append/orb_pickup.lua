c1dbb83339de7f844f269a857db752f94ea14df1
Kabby <1925023+tehkab@users.noreply.github.com>
2024-06-20 22:37:09 -0400

    Add game rule / flag "NT_sync_orbs_no_curse" to not send cursed hearts
    This needs an option added to the client to work!

--- noita_mod/core/files/append/orb_pickup.lua
+++ noita_mod/core/files/append/orb_pickup.lua
@@ -19,6 +19,17 @@ end
 
 --hook EntityLoad to inject fake_orb tag onto spawned heart
 function EntityLoad(filename, pos_x, pos_y)
+    --in "no curse" mode, replace fake orb's cursed hearts with real hearts (anti griefing option)
+    --but the originator's (orb has no fake_orb tag) cursed heart remains real
+    if GameHasFlagRun("NT_sync_orbs_no_curse") and filename == "data/entities/items/pickup/heart_evil.xml" then
+        --not a huge fan of doing it this way but other options are uglier
+        local orbs = EntityGetInRadiusWithTag(pos_x, pos_y, 1, "fake_orb") --should be at exactly same x,y position
+        if #orbs > 0 then --probably never 0 or 1
+            --replace with a normal heart; will get fake_orb tag down below
+            filename = "data/entities/items/pickup/heart.xml"
+        end
+    end
+
     local entity_id = _EntityLoad(filename, pos_x, pos_y)
     if filename == "data/entities/items/pickup/heart.xml" then
         EntityAddTag(entity_id,"fake_orb")
