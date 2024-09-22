5733bcc0b7fbe59349d5a55538434ffb64635bbf
Skye Telman <b.telman01@gmail.com>
2024-09-21 22:44:33 -0400

    Update lua version

--- noita_mod/core/files/ws/events.lua
+++ noita_mod/core/files/ws/events.lua
@@ -114,7 +114,7 @@ wsEvents = {
     RequestGameInfo = function(data)
         local seed = StatsGetValue("world_seed")
         local mods = ModGetActiveModIDs()
-        SendWsEvent({ event = "GameInfo", payload = { seed = seed, mods = mods, version = "v0.19.0", beta = GameIsBetaBuild() } })
+        SendWsEvent({ event = "GameInfo", payload = { seed = seed, mods = mods, version = "v0.20.0", beta = GameIsBetaBuild() } })
         SendWsEvent({ event = "RequestPlayerList", payload = {} })
         PopulateSpellList()
     end,
